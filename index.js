181ab5e1f222aac05df2925fe87edaff8917c9bf
Faruk Çakal <fcakal07@gmail.com>
2024-10-08 16:35:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new SteamUser();
 
 const username = process.env.STEAM_USERNAME;
 const password = process.env.STEAM_PASSWORD;
-const gameId = process.env.STEAM_GAME_ID;
+const gameId = parseInt(process.env.STEAM_GAME_ID, 10); // App ID'yi tamsayıya çevir
 
 client.logOn({
     accountName: username,
@@ -15,7 +15,7 @@ client.logOn({
 client.on('loggedOn', () => {
     console.log('Giriş yapıldı!');
     
-    startGame(client);
+    startGame(client); // Oyun başlat
 });
 
 client.on('chatMessage', (steamID, message) => {
@@ -24,10 +24,10 @@ client.on('chatMessage', (steamID, message) => {
     // Mesajı kontrol et
     if (message.toLowerCase() === 'start') {
         console.log('Oyun başlatılıyor...');
-        startGame()
+        startGame(client);
     } else if (message.toLowerCase() === 'stop') {
         console.log('Oyun durduruluyor...');
-        stopGame()
+        stopGame(client);
     }
 });
 
@@ -48,23 +48,13 @@ client.on('error', (err) => {
 });
 
 const startGame = (client) => {
-    // client.playingState = {
-    //     blocked: false,
-    //     appid: [gameId]
-    // }
+    client.setPersona(SteamUser.EPersonaState.Online); // Çevrimiçi durumu ayarla
 
-    client.setPersona(SteamUser.EPersonaState.Online)
-
-    client.gamesPlayed([gameId]);
+    // Oyun bilgilerini ayarla
+    client.gamesPlayed([gameId]); // Oyun oynamaya başla
 }
 
 const stopGame = (client) => {
-    // client.playingState = {
-    //     blocked: false,
-    //     appid: []
-    // }
-
-    client.setPersona(SteamUser.EPersonaState.Offline)
-
-    client.gamesPlayed([]);
+    client.setPersona(SteamUser.EPersonaState.Offline); // Çevrimdışı durumu ayarla
+    client.gamesPlayed([]); // Oyun oynamayı durdur
 }
